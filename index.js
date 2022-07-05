d1f07f4fa53ec0f8c603f6afc1b92bd0dff77a58
Abhijeet <abhijeet3469@gmail.com>
2022-07-05 19:12:23 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const middleware1 = function (req, res, next) {
+  console.log("middleware1");
+  next();
+};
+const middleware2 = function (req, res, next) {
+  console.log("middleware2");
+  next();
+};
+app.use(middleware2);
+app.get("/one", middleware1, function (req, res) {
+  console.log("md1");
+  res.send("one");
+});
+app.get("/two", function (req, res) {
+  console.log("md2");
+  res.send("two");
+});
+app.get("/three", function (req, res) {
+  console.log("md3");
+  res.send("three");
+});
+app.listen(4000);
